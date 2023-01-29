4912006739efaa3a99add81a7d469c6916f9a27d
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-01-28 22:27:45 -0600

    Added NeoTree Configs & rounded for NeoTree

---
+++ lua/plugins/configs/neotree.lua
@@ -0,0 +1,17 @@
+local status_ok, ntree = pcall(require, "neo-tree")
+
+if not status_ok then
+	return
+end
+
+ntree.setup({
+	popup_border_style = "rounded",
+	enable_git_status = true,
+	icon = {
+		folder_closed = "",
+		folder_open = "",
+		folder_empty = "ﰊ",
+		default = "*",
+		highlight = "NeoTreeFileIcon",
+	},
+})
