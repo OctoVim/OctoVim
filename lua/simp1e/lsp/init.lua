db39e445dee46afd91ed3c1dfdeda3bc3386fef4
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-01-30 18:48:02 -0600

    removed unused files

--- lua/simp1e/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
-require("simp1e.lsp.mason")
-require("simp1e.lsp.handlers").setup()
-require("simp1e.lsp.null-ls")
-require("simp1e.lsp.lspsaga")
